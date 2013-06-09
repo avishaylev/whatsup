bd89d989228dcc35c2f9b714530868c0384825f9
Ryan Wagner <rwagner@spyderbytedesign.com>
2013-06-09 14:38:50 -0400

    Added the functions and variables to the FourSquareConnect interface.

--- FourSquareConnect.php
+++ FourSquareConnect.php
@@ -1,2 +1,32 @@
 <?php
 
+/**
+ * Provides base functions and attributes for most other classes
+ * that we're going to implement
+ *
+ * @package WhatsUp
+ * @author Ryan & Reese
+ **/
+interface FourSquareConnect
+{
+
+  var $Client_id;
+  var $Client_secret;
+  var $Push_secret;
+
+
+  protected function setURL()
+  {
+
+  }
+
+  protected function request()
+  {
+    
+  }
+
+  protected function getReponse()
+  {
+    
+  }
+} // END interface FourSquareConnect
